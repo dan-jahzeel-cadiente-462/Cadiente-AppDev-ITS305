fb29f98b355da04cbad8c8c21b7604b646e081de
Dan Jahzeel Cadiente <dcadiente462@gmail.com>
2026-04-21 08:25:46 +0800

    Initial commit

--- src/utils/image.js
+++
@@ -1,9 +0,0 @@
-const imageUtils = {
-  // Add your image utilities here if needed
-  getImageSource: (imageName) => {
-    // Example utility function
-    return require(`../assets/${imageName}`);
-  },
-};
-
-export default imageUtils;
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export { default as imageUtils } from './image';
-export * from './routes';
